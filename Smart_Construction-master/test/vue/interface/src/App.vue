537a581f1f51eee2d239a28a98da688e4e8c339f
ZYQ123122 <23231234@bjtu.edu.cn>
2025-07-09 16:14:17 +0800

    测试界面

---
+++ Smart_Construction-master/test/vue/interface/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button>导入视频</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #369a6b;
+}
+</style>
